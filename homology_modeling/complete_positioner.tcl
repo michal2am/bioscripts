995653de17361e20f28ea5b264d43e4da582b6d1
michal2am <michaladammichalowski@gmail.com>
2015-11-02 10:20:53 +0100

    clean up

--- homology_modeling/complete_positioner.tcl
+++
@@ -1,6 +0,0 @@
-mol load pdb ../hybrid_models/hybrid_4cof_3rif/modeller_models_many/gabar_MM.01.B99990035_fit.pdb 
-source orienter.tcl
-source centerer.tcl
-[atomselect top all] writepdb model_35_fit_pos.pdb
-mol delete top
-resetpsf
