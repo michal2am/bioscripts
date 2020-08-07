f827ac61d34c7e4d49d12dc59a9a3b0a487818d6
michal_2am <michaladammichalowski@gmail.com>
2020-08-07 12:24:00 +0200

    macroscopic trend modeling auto mode

--- single_channel/analytic_macro.py
+++ single_channel/analytic_macro.py
@@ -5,27 +5,103 @@ import pandas as pd
 import plotly.express as px
 from scalcs import mechanism
 from scalcs import cjumps
+import argparse
 
 
-class ModelCCOD:
+class Model:
 
-    def __init__(self, rates):
+    def __init__(self, topology, rates):
         """
 
         :param rates:
         """
-
-        self.mechanism = rates
+        self.model_mechanism = (topology, rates)
 
     @property
-    def mechanism(self):
-        return self._mechanism
+    def model_mechanism(self):
+        return self._model_mechanism
 
-    @mechanism.setter
-    def mechanism(self, rates):
+    @model_mechanism.setter
+    def model_mechanism(self, params):
 
-        mectitle = 'C-C-O-D'
-        ratetitle = 'quasi random numbers'
+        if params[0] == 'RAAFOODD':
+            self._model_mechanism = self.mechanism_RAAFOODD(params[1])
+
+        elif params[0] == 'RAAFODD':
+            self._model_mechanism = self.mechanism_RAAFODD(params[1])
+
+        elif params[0] == 'RAAFOD':
+            self._model_mechanism = self.mechanism_RAAFOD(params[1])
+
+    def mechanism_RAAFOD(self, rates):
+
+        mectitle = 'R-A-A2-F-0=D'
+        ratetitle = 'starters'
+
+        A2D = mechanism.State('B', 'A2D', 0.0)
+        A2O = mechanism.State('A', 'A2O', 50e-12)
+        A2F = mechanism.State('B', 'A2F', 0.0)
+        A2R = mechanism.State('B', 'A2R', 0.0)
+        AR = mechanism.State('B', 'AR', 0.0)
+        R = mechanism.State('C', 'R', 0.0)
+
+        RateList = [
+
+            mechanism.Rate(rates['res'], A2D, A2F, name='res', limits=[1e-15, 1e+7]),
+            mechanism.Rate(rates['des'], A2F, A2D, name='des', limits=[1e-15, 1e+7]),
+
+            mechanism.Rate(rates['alpha'], A2O, A2F, name='alpha', limits=[1e-15, 1e+7]),
+            mechanism.Rate(rates['beta'], A2F, A2O, name='beta', limits=[1e-15, 1e+7]),
+
+            mechanism.Rate(rates['gamma'], A2F, A2R, name='gamma', limits=[1e-15, 1e+7]),
+            mechanism.Rate(rates['delta'], A2R, A2F, name='delta', limits=[1e-15, 1e+7]),
+
+            mechanism.Rate(rates['koff'] * 2, A2R, AR, name='2koff', limits=[1e-15, 1e+7]),
+            mechanism.Rate(rates['kon'] * 2, R, AR, name='2kon', eff='c', limits=[1e-15, 1e+10]),
+            mechanism.Rate(rates['koff'], AR, R, name='koff', limits=[1e-15, 1e+7]),
+            mechanism.Rate(rates['kon'], AR, A2R, name='kon', eff='c', limits=[1e-15, 1e+10]),
+        ]
+
+        return mechanism.Mechanism(RateList, mtitle=mectitle, rtitle=ratetitle)
+
+    def mechanism_RAAFODD(self, rates):
+
+        mectitle = 'R-A-A2-F-0=2D'
+        ratetitle = 'starters'
+
+        A2Dp = mechanism.State('B', 'A2Dp', 0.0)
+        A2D = mechanism.State('B', 'A2D', 0.0)
+        A2O = mechanism.State('A', 'A2O', 50e-12)
+        A2F = mechanism.State('B', 'A2F', 0.0)
+        A2R = mechanism.State('B', 'A2R', 0.0)
+        AR = mechanism.State('B', 'AR', 0.0)
+        R = mechanism.State('C', 'R', 0.0)
+
+        RateList = [
+
+            mechanism.Rate(rates['res'], A2D, A2F, name='res', limits=[1e-15, 1e+7]),
+            mechanism.Rate(rates['des'], A2F, A2D, name='des', limits=[1e-15, 1e+7]),
+            mechanism.Rate(rates['res_p'], A2Dp, A2F, name='res_p', limits=[1e-15, 1e+7]),
+            mechanism.Rate(rates['des_p'], A2F, A2Dp, name='des_p', limits=[1e-15, 1e+7]),
+
+            mechanism.Rate(rates['alpha'], A2O, A2F, name='alpha', limits=[1e-15, 1e+7]),
+            mechanism.Rate(rates['beta'], A2F, A2O, name='beta', limits=[1e-15, 1e+7]),
+
+            mechanism.Rate(rates['gamma'], A2F, A2R, name='gamma', limits=[1e-15, 1e+7]),
+            mechanism.Rate(rates['delta'], A2R, A2F, name='delta', limits=[1e-15, 1e+7]),
+
+            mechanism.Rate(rates['koff'] * 2, A2R, AR, name='2koff', limits=[1e-15, 1e+7]),
+            mechanism.Rate(rates['kon'] * 2, R, AR, name='2kon', eff='c', limits=[1e-15, 1e+10]),
+            mechanism.Rate(rates['koff'], AR, R, name='koff', limits=[1e-15, 1e+7]),
+            mechanism.Rate(rates['kon'], AR, A2R, name='kon', eff='c', limits=[1e-15, 1e+10]),
+        ]
+
+        return mechanism.Mechanism(RateList, mtitle=mectitle, rtitle=ratetitle)
+
+    def mechanism_RAAFOODD(self, rates):
+
+        mectitle = 'R-A-A2-F-20=2D'
+        ratetitle = 'starters'
 
         A2Dp = mechanism.State('B', 'A2Dp', 0.0)
         A2D  = mechanism.State('B', 'A2D', 0.0)
@@ -57,73 +133,188 @@ class ModelCCOD:
              mechanism.Rate(rates['kon'], AR, A2R, name='kon', eff='c', limits=[1e-15,1e+10]),
              ]
 
-        self._mechanism = mechanism.Mechanism(RateList, mtitle=mectitle, rtitle=ratetitle)
+        return mechanism.Mechanism(RateList, mtitle=mectitle, rtitle=ratetitle)
 
 
-def to_pc(header, outfile, multi_data):
+class ModelsBuilder:
+
+    # def __init__(self, s_rates, v_rate, topo):
+    def __init__(self, s_rates, topo):
+
+        self.topology = topo
+        self.start_rates = s_rates
+        # self.variable_rate = v_rate
+
+    @property
+    def topology(self):
+        return self._topology
+
+    @topology.setter
+    def topology(self, topo):
+        self._topology = topo
+
+    @property
+    def start_rates(self):
+        return self._start_rates
+
+    @start_rates.setter
+    def start_rates(self, s_rates):
+        self._start_rates = s_rates
+
     '''
-    saves Pandas data frame as single atf file
-    :param header: path to header template
-    :param outfile: path to atf outfile
-    :param multi_data: Pandas data frame with all the abfs data
-    :return:
+    @property
+    def variable_rate(self):
+        return self._variable_rate
+
+    @variable_rate.setter
+    def variable_rate(self, v_rate):
+        self._variable_rate = v_rate
     '''
 
-    # prepare header
-    with open(header, 'r') as h:
-        cols = multi_data.shape[1]
+    '''
+    def build_models(self):
 
-        header = h.readlines()
-        header[1] = "7\t{}\n".format(1 + cols)
-        header[8] = header[8].rstrip() + "\t" + "\"sim #1\"\t" * cols + "\n"
-        header[9] = header[9].rstrip() + "\t" + "\"Trace #1 (prob)\"\t" * cols + "\n"
-        header = ''.join(header)
+        step_size = 8e-5
+        record_length = 1000e-3
 
-    # write header
-    with open(outfile, 'w') as d:
-        d.write(header)
+        model_trace = pd.DataFrame()
 
-    # write all data
-    with open(outfile, 'a') as d:
-        multi_data.to_csv(d, header=False, float_format='%.4f', sep='\t')
+        for variable_rate_val in [0.1 * self.start_rates[self.variable_rate], 0.3 * self.start_rates[self.variable_rate],
+                                  0.5 * self.start_rates[self.variable_rate], 0.7 * self.start_rates[self.variable_rate],
+                                  0.9 * self.start_rates[self.variable_rate],
+                                  self.start_rates[self.variable_rate],
+                                  1.1 * self.start_rates[self.variable_rate], 1.3 * self.start_rates[self.variable_rate],
+                                  1.5 * self.start_rates[self.variable_rate], 1.7 * self.start_rates[self.variable_rate],
+                                  2.0 * self.start_rates[self.variable_rate]]:
+
+            self.start_rates[self.variable_rate] = variable_rate_val
+            sample_model = Model(self.topology, start_rates)
+
+            t, c, Popen, P = cjumps.solve_jump(sample_model.model_mechanism, record_length, step_size, cjumps.pulse_square,
+                                               (1e-2, 0.0, 100e-3, 500e-3))
+
+            model_trace[variable_rate_val] = Popen
+
+        model_trace['t'] = t * 1000
+        model_trace.set_index(keys='t', drop=True, inplace=True)
+
+        model_trace_normalized = model_trace.divide(model_trace.max(), axis=1)
+        model_trace_normalized_forATF = model_trace_normalized.copy()
+        print(model_trace_normalized_forATF)
+
+        model_trace_normalized.reset_index(inplace=True)
+        model_trace_normalized_long = (
+            model_trace_normalized.melt(id_vars=['t'], var_name=variable_rate, value_name='Popen'))
+
+        fig = px.line(model_trace_normalized_long, x='t', y='Popen', color=variable_rate,
+                      color_discrete_sequence=px.colors.qualitative.Dark24, title=str(start_rates))
+        fig.write_html(self.variable_rate + '.html')
+
+        self.save_atf('header.txt', self.variable_rate + '.atf', model_trace_normalized_forATF)
+    '''
+
+    def build_models_multi(self):
+
+        step_size = 8e-5
+        record_length = 1000e-3
+        model_traces = []
+
+        for variable_rate in self.start_rates.keys():
+
+            steps = [0.1, 0.3, 0.5, 0.7, 0.9, 1.0, 1.1, 1.3, 1.5, 2.0, 5.0]
+
+            single_var_trace_atf = pd.DataFrame()
+
+            for step in steps:
+
+                variable_rate_val = self.start_rates[variable_rate] * step
+
+                current_rates = self.start_rates.copy()
+                current_rates[variable_rate] = variable_rate_val
+                sample_model = Model(self.topology, current_rates)
+
+                t, c, Popen, P = cjumps.solve_jump(sample_model.model_mechanism, record_length, step_size,
+                                                   cjumps.pulse_square, (1e-2, 0.0, 100e-3, 500e-3))
+
+                model_trace = pd.DataFrame()
+
+                model_trace['Popen'] = Popen
+                model_trace = model_trace.divide(model_trace.max(), axis=1)
+                model_trace['t'] = t * 1000
+
+                model_trace['Rate_name'] = variable_rate
+                model_trace['Rate_value'] = variable_rate_val
+                model_trace['Rate_value_step'] = step
+
+                print(variable_rate, variable_rate_val)
+                print(model_trace)
+                model_traces.append(model_trace)
+
+                single_var_trace_atf[variable_rate_val] = Popen
+
+            single_var_trace_atf['t'] = t * 1000
+            single_var_trace_atf.set_index(keys='t', drop=True, inplace=True)
+            self.save_atf('header.txt', variable_rate + '.atf', single_var_trace_atf)
+
+        all_traces = pd.concat(model_traces)
+
+        fig = px.line(all_traces, x='t', y='Popen', color='Rate_value_step', facet_col='Rate_name', facet_col_wrap=2,
+                      height=500 * len(self.start_rates.keys()),
+                      template='simple_white',
+                      color_discrete_sequence=px.colors.sequential.RdBu,
+                      hover_data=['Rate_value'])
+
+        fig.update_layout(font=dict(family="Courier New, monospace", size=18))
+
+        fig.write_html('simulations' + '.html')
+
+    def save_atf(self, header, outfile, multi_data):
+        '''
+        saves Pandas data frame as single atf file
+        :param header: path to header template
+        :param outfile: path to atf outfile
+        :param multi_data: Pandas data frame with all the abfs data
+        :return:
+        '''
+
+        # prepare header
+        with open(header, 'r') as h:
+            cols = multi_data.shape[1]
+
+            header = h.readlines()
+            header[1] = "7\t{}\n".format(1 + cols)
+            header[8] = header[8].rstrip() + "\t" + "\"sim #1\"\t" * cols + "\n"
+            header[9] = header[9].rstrip() + "\t" + "\"Trace #1 (prob)\"\t" * cols + "\n"
+            header = ''.join(header)
+
+        # write header
+        with open(outfile, 'w') as d:
+            d.write(header)
+
+        # write all data
+        with open(outfile, 'a') as d:
+            multi_data.to_csv(d, header=False, float_format='%.4f', sep='\t')
 
 
-start_rates = {'kon': 10e6, 'koff': 900,
-               'delta': 4400, 'gamma': 3000,
-               'beta': 6900, 'alpha': 1800, 'beta_p': 4300, 'alpha_p': 450,
-               'des': 4000, 'res': 60, 'des_p': 400, 'res_p': 3}
-
-step_size = 8e-5
-record_length = 1000e-3
-model_trace = pd.DataFrame()
-
-variable_rate = 'delta'
-
-for variable_rate_val in [0.1*start_rates[variable_rate], 0.3*start_rates[variable_rate], 0.5*start_rates[variable_rate], 0.7*start_rates[variable_rate], 0.9*start_rates[variable_rate],
-                          start_rates[variable_rate],
-                          1.1*start_rates[variable_rate], 1.3*start_rates[variable_rate], 1.5*start_rates[variable_rate], 1.7*start_rates[variable_rate], 2.0*start_rates[variable_rate]]:
-
-    start_rates[variable_rate] = variable_rate_val
-    sample_model = ModelCCOD(start_rates)
-
-    t, c, Popen, P = cjumps.solve_jump(sample_model.mechanism, record_length, step_size, cjumps.pulse_square, (1e-2, 0.0, 100e-3, 500e-3))
-
-    model_trace[variable_rate_val] = Popen
-
-model_trace['t'] = t * 1000
-model_trace.set_index(keys='t', drop=True, inplace=True)
-
-model_trace_normalized = model_trace.divide(model_trace.max(), axis=1)
-model_trace_normalized_forATF = model_trace_normalized.copy()
+parser = argparse.ArgumentParser()
+# parser.add_argument("-vr", "--variable_rate", type=str)
+parser.add_argument("-sr", "--start_rates", type=str)
+parser.add_argument("-tp", "--topology", type=str)
 
 
-model_trace_normalized.reset_index(inplace=True)
-model_trace_normalized_long = (model_trace_normalized.melt(id_vars=['t'], var_name=variable_rate, value_name='Popen'))
+args = parser.parse_args()
 
 
-print(model_trace_normalized_forATF)
+start_rates = pd.read_csv(args.start_rates, header=None)
+start_rates = dict(zip(start_rates[0], start_rates[1]))
+# variable_rate = args.variable_rate
 
-fig = px.line(model_trace_normalized_long, x='t', y='Popen', color=variable_rate, color_discrete_sequence=px.colors.qualitative.Dark24, title=str(start_rates))
-fig.write_html('test.html')
+# builder = ModelsBuilder(start_rates, variable_rate, args.topology)
 
-to_pc('header.txt', 'test.atf', model_trace_normalized_forATF)
+builder = ModelsBuilder(start_rates, args.topology)
+#builder.build_models()
+builder.build_models_multi()
+
+
+
+
