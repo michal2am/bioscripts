7611c98f3d8a898500343291af613a990ea4af2b
michal_2am <michaladammichalowski@gmail.com>
2020-11-20 14:13:36 +0100

    some old stuff with scn listing (lab)

--- single_channel/fit_scnlist.py
+++ single_channel/fit_scnlist.py
@@ -3,5 +3,6 @@ import pandas as pd
 # find . -name \*.SCN -printf '%p\n' > scn_list.csv
 # find . -name \*.SCN -exec cp {} workspace_hjcfit \;
 scn_list = pd.read_csv('scn_list.csv', sep='/', header=None)
+print(scn_list)
 com_scn = scn_list.iloc[:, -2:]
 com_scn.to_csv('scn_list_cells.csv')
