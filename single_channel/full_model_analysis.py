1dd0f2f6dd3dbf5a617e03949419ad8f194a6aee
michal_2am <michaladammichalowski@gmail.com>
2021-07-05 14:54:15 +0200

    full model mining

--- single_channel/full_model_analysis.py
+++ single_channel/full_model_analysis.py
@@ -8,6 +8,7 @@ from sklearn.datasets import make_blobs
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 from sklearn.preprocessing import StandardScaler
+import plotly.express as px
 
 #rates = rates.melt(id_vars=['project', 'type'])
 
@@ -39,15 +40,43 @@ hists.map(sns.kdeplot, 'value')
 plt.show()
 '''
 
-# clusters
+# k-means
+
+'''
 rates = pd.read_csv('published_rates_noPK_WTmean.csv')
 rates = rates.set_index('type')
 print(rates)
 
+selected_rates = ['d2', 'r2']
+rates_names = ['gamma', 'delta', 'alpha2', 'beta2', 'alpha2p', 'beta2p', 'd2', 'r2', 'd2p', 'r2p']
+to_drop = [rate for rate in rates_names if rate not in selected_rates]
+
 rates.drop('project', axis=1, inplace=True)
-rates.drop(['gamma', 'delta', 'alpha2p', 'beta2p', 'd2', 'r2', 'd2p', 'r2p'], axis=1, inplace=True)
+rates.drop(to_drop, axis=1, inplace=True)
+
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
+    kmeans.fit(rates)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1, 11), wcss)
+plt.title('Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+kmeans = KMeans(n_clusters=3, init='k-means++', max_iter=300, n_init=10, random_state=0)
+kmeans.fit(rates)
+labels = kmeans.predict(rates)
+rates['cluster'] = labels
 
 print(rates)
+
+ax = sns.scatterplot(data=rates, x=selected_rates[0], y=selected_rates[1], hue="cluster")
+for line in range(0,rates.shape[0]):
+     ax.text(rates[selected_rates[0]][line], rates[selected_rates[1]][line], rates.index[line], horizontalalignment='center', size='medium', color='black', weight='semibold')
+plt.show()
+'''
 
 def kmeans_missing(X, n_clusters, max_iter=10):
     """Perform K-Means clustering on data with missing values.
@@ -97,31 +126,40 @@ def kmeans_missing(X, n_clusters, max_iter=10):
 
 # missing k-means
 '''
-labels, centroids, X_hat = kmeans_missing(rates, 4)
+
+rates = pd.read_csv('published_rates_noPK_WTmean.csv')
+rates = rates.set_index('type')
+rates.drop('project', axis=1, inplace=True)
+
+
+print(rates)
+
+labels, centroids, X_hat = kmeans_missing(rates, 6)
 rates['cluster'] = labels
 print(rates)
 '''
-# regular k-means
+selected_rates = ['delta', 'gamma']
+selected_project = 'P273'
 
-wcss = []
-for i in range(1, 11):
-    kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
-    kmeans.fit(rates)
-    wcss.append(kmeans.inertia_)
-plt.plot(range(1, 11), wcss)
-plt.title('Elbow Method')
-plt.xlabel('Number of clusters')
-plt.ylabel('WCSS')
-plt.show()
+rates = pd.read_csv('published_rates_noPK_WTmean.csv')
+selected = rates[rates['project'].isin(['WT', selected_project])]
+selected['forward'] = np.log(selected[selected_rates[0]])
+selected['equilibrium'] = np.log(selected[selected_rates[0]]/selected[selected_rates[1]])
 
-kmeans = KMeans(n_clusters=3, init='k-means++', max_iter=300, n_init=10, random_state=0)
-kmeans.fit(rates)
-labels = kmeans.predict(rates)
-rates['cluster'] = labels
+print(selected)
 
-print(rates)
+project_cumuCells = px.scatter(selected,
+                               x='equilibrium', y='forward',
+                               title=selected_project,
+                               color='type', template='presentation', width=800, height=800,
+                               marginal_x='rug', marginal_y='rug',
+                               color_discrete_sequence=px.colors.qualitative.Dark24,
+                               )
+project_cumuCells.add_trace(
+    px.scatter(selected, x='equilibrium', y='forward',
+               trendline='ols',
+               color_discrete_sequence=px.colors.qualitative.Dark24,
+               ).data[1])
 
-ax = sns.scatterplot(data=rates, x="beta2", y="alpha2", hue="cluster")
-for line in range(0,rates.shape[0]):
-     ax.text(rates.beta2[line], rates.alpha2[line], rates.index[line], horizontalalignment='center', size='medium', color='black', weight='semibold')
-plt.show()
+project_cumuCells.write_html(selected_project + '_plot.html')
+
