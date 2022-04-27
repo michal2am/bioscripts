8c0abf98baa804cb0dbf638dd29a1a052bbda456
michal2am <michaladammichalowski@gmail.com>
2022-04-27 18:00:58 +0200

    corrected selections

--- stockholm/mdanalysis/lipid_detector.py
+++ stockholm/mdanalysis/lipid_detector.py
@@ -13,6 +13,7 @@ parser.add_argument("--dir")    # full path to the dir with dir prefix /xxx/yyy/
 parser.add_argument("--num", type=int)  # replica number
 parser.add_argument("--l_type")     # ligand name
 parser.add_argument("--l_state")    # apo/holo
+parser.add_argument("--rad")    # sphere detector radius
 
 args = parser.parse_args()
 
@@ -25,6 +26,7 @@ xtc = dir_name + str(int(dir_num)) + '/' + xtc_f
 ligand_type = args.l_type
 ligand_state = args.l_state
 chain_order = args.ord
+radius = args.rad
 
 
 class SubunitInterface:
@@ -32,14 +34,15 @@ class SubunitInterface:
     object to store the data, nothing using the mda universe as it is
     """
 
-    def __init__(self, interface_name,  system, l_type, l_state, lipid_contact_selection=None):
+    def __init__(self, interface_name,  system, l_type, l_state, rad, lipid_contact_selection=None):
         self.interface_name = interface_name
         self.system = system
         self.ligand_type = l_type
         self.lignad_state = l_state
 
         # only for lipids:
-        self.lipids_selection = '((resname POPC) and sphzone 5.0 {})'.format(lipid_contact_selection)
+        self.det_rad = rad
+        self.lipids_selection = '((resname POPC) and sphzone {} {})'.format(self.det_rad, lipid_contact_selection)
         self.lipids_contacts = []
 
     def data(self):
@@ -49,17 +52,18 @@ class SubunitInterface:
         data['interface'] = self.interface_name
         data['ligand_type'] = self.ligand_type
         data['ligand_state'] = self.lignad_state
+        data['detection_radius'] = self.det_rad
         return data
 
 
 if chain_order == 'ACBDE':
-    interface_configs = [['1st_beta/alpha', '((segid A and resid 289) or (segid A and resid 265) or (segid C and resid 233))'],
-                         ['2nd_beta/alpha', '((segid B and resid 289) or (segid B and resid 265) or (segid D and resid 233))'],
+    interface_configs = [['1st_beta/alpha', '(name CA and ((segid A and resid 289) or (segid A and resid 265) or (segid C and resid 233)))'],
+                         ['2nd_beta/alpha', '(name CA and ((segid B and resid 289) or (segid B and resid 265) or (segid D and resid 233)))'],
 
-                         ['alpha/beta', '((segid C and resid 294) or (segid C and resid 270) or (segid B and resid 228))'],
-                         ['alpha/gamma', '((segid D and resid 294) or (segid D and resid 270) or (segid E and resid 243))'],
+                         ['alpha/beta', '(name CA and ((segid C and resid 294) or (segid C and resid 270) or (segid B and resid 228)))'],
+                         ['alpha/gamma', '(name CA and ((segid D and resid 294) or (segid D and resid 270) or (segid E and resid 243)))'],
 
-                         ['gamma/beta', '((segid E and resid 304) or (segid E and resid 280) or (segid A and resid 228))']]
+                         ['gamma/beta', '(name CA and ((segid E and resid 304) or (segid E and resid 280) or (segid A and resid 228)))']]
     '''
     # original:
     interface_configs = [['1st_beta/alpha', '((segid A and resid 289) or (segid A and resid 286) or '
@@ -75,13 +79,13 @@ if chain_order == 'ACBDE':
     '''
 
 if chain_order == 'ABCDE':
-    interface_configs = [['1st_beta/alpha', '((segid A and resid 289) or (segid A and resid 265) or (segid B and resid 233))'],
-                         ['2nd_beta/alpha', '((segid C and resid 289) or (segid C and resid 265) or (segid D and resid 233))'],
+    interface_configs = [['1st_beta/alpha', '(name CA and ((segid A and resid 289) or (segid A and resid 265) or (segid B and resid 233)))'],
+                         ['2nd_beta/alpha', '(name CA and ((segid C and resid 289) or (segid C and resid 265) or (segid D and resid 233)))'],
 
-                         ['alpha/beta', '((segid B and resid 294) or (segid B and resid 270) or (segid C and resid 228))'],
-                         ['alpha/gamma', '((segid D and resid 294) or (segid D and resid 270) or (segid E and resid 243))'],
+                         ['alpha/beta', '(name CA and ((segid B and resid 294) or (segid B and resid 270) or (segid C and resid 228)))'],
+                         ['alpha/gamma', '(name CA and ((segid D and resid 294) or (segid D and resid 270) or (segid E and resid 243)))'],
 
-                         ['gamma/beta', '((segid E and resid 304) or (segid E and resid 280) or (segid A and resid 228))']]
+                         ['gamma/beta', '(name CA and ((segid E and resid 304) or (segid E and resid 280) or (segid A and resid 228)))']]
     '''
     # original:
     interface_configs = [['1st_beta/alpha', '((segid A and resid 289) or (segid A and resid 286) or '
@@ -100,7 +104,8 @@ if chain_order == 'ABCDE':
 universe = MDAnalysis.Universe(pdb, xtc)
 
 # create data container for each subunit interface
-all_interfaces = [SubunitInterface(interface_config[0], 'MD' + str(dir_num), ligand_type, ligand_state, interface_config[1])
+all_interfaces = [SubunitInterface(interface_config[0], 'MD' + str(dir_num), ligand_type, ligand_state,
+                                   radius, interface_config[1])
                   for interface_config in interface_configs]
 
 # fill each container with data while going through trajectory
@@ -116,4 +121,4 @@ for interface in all_interfaces:
     all_complete = pd.concat([all_complete, interface.data()])
 
 all_complete.index.name = 'frame'
-all_complete.to_csv('{}_{}_MD{}_lipids.csv'.format(ligand_type, ligand_state, dir_num))
+all_complete.to_csv('{}_{}_{}_MD{}_lipids.csv'.format(ligand_type, ligand_state, radius, dir_num))
