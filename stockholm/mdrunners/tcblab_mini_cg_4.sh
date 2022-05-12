958dbf6cf9b5679edf3921f16cb338cf3d48e343
michal2am <michaladammichalowski@gmail.com>
2022-05-12 17:14:08 +0200

    slrum scripts stuff

--- stockholm/mdrunners/tcblab_mini_cg_4.sh
+++ stockholm/mdrunners/tcblab_mini_cg_4.sh
@@ -12,19 +12,29 @@ sys=1
 sysmax=4
 
 # minimization step 0
-module unload gromacs
-module load gromacs/2021.5
+# needs to be done using double precision
+# TODO: make it parallel
 
 while (($sys <= $sysmax))
 do
         cd sys${sys}/
+
+        module unload gromacs
+        module load gromacs/2021.5
+
         gmx grompp -f ../mdp/step6.0_minimization.mdp -o step6.0_minimization.tpr -c ../step5_charmm2gmx.pdb -r ../step5_charmm2gmx.pdb -p ../system.top -n ../index.ndx -maxwarn -1
+
+        module unload gromacs
+        module load gromacs/2021.5 gromacs=gmx_d
+
+        gmx_d mdrun -v -deffnm step6.0_minimization
+
         cd ../
         sys=$((sys+1))
 done
 
 module unload gromacs
-module load gromacs/2021.5 gromacs=gmx_mpi
+module load gromacs/2021.5
 
 mpirun -np 4 gmx_mpi mdrun -v -deffnm step6.0_minimization -multidir sys1/ sys2/ sys3/ sys4/
 
@@ -38,7 +48,7 @@ module load gromacs/2021.5
 while (($sys <= $sysmax))
 do
         cd sys${sys}/
-        gmx grompp -f ../mdp/step6.1_minimization.mdp -o step6.1_minimization.tpr -c ../step5_charmm2gmx.pdb -r ../step5_charmm2gmx.pdb -p ../system.top -n ../index.ndx -maxwarn -1
+        gmx grompp -f ../mdp/step6.1_minimization.mdp -o step6.1_minimization.tpr -c step6.0_minimization.gro -r ../step5_charmm2gmx.pdb -p ../system.top -n ../index.ndx -maxwarn -1
         cd ../
         sys=$((sys+1))
 done
