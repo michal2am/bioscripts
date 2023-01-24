9c162d6e53d6ce27f2586b725a66f4009eccb5c0
michal2am <michaladammichalowski@gmail.com>
2023-01-24 13:37:32 +0100

    scn reader

--- single_channel/ideal&files/scn2csv.py
+++ single_channel/ideal&files/scn2csv.py
@@ -3,12 +3,20 @@ import numpy as np
 from dcpyps import dcio
 import seaborn as sns
 import matplotlib.pyplot as plt
+import pyabf
 
-scn_file_name = '89145K1.SCN'
+abf = pyabf.ABF("89142k1.abf")
+abf.setSweep(0)
+#plt.plot(abf.sweepX, abf.sweepY)
+#plt.show()
+
+
+scn_file_name = '89142K1.SCN'
 
 header = dcio.scn_read_header(scn_file_name)
 scan = dcio.scn_read_data(scn_file_name, header[3])     # header[3] seems to contain needed dict with the parameters
 scan = pd.DataFrame({'period_scan': scan[0], 'scan_amp': scan[1], 'scan_cat': scan[-1]})
+
 
 """    
     0 = all OK;
@@ -21,15 +29,17 @@ scan = pd.DataFrame({'period_scan': scan[0], 'scan_amp': scan[1], 'scan_cat': sc
 # print(scan[scan['scan_cat'] == 8])
 
 # normalized amplitude
-scan.loc[:, 'scan_amp_n'] = ((scan.loc[:, 'scan_amp'] - min(scan.loc[:, 'scan_amp']))/min(scan.loc[:, 'scan_amp']))
+# scan.loc[:, 'scan_amp_n'] = ((scan.loc[:, 'scan_amp'] - min(scan.loc[:, 'scan_amp']))/min(scan.loc[:, 'scan_amp']))
 # just binary 0 or 1 amplitudes
 scan.loc[scan['scan_amp'] != 0, 'scan_amp_b'] = 1
 scan.loc[scan['scan_amp'] == 0, 'scan_amp_b'] = 0
+scan['period_scan'] = scan['period_scan']*1000
 
 # adding up series of neighbouring shut or open dwell times
 start_event_no = 0
 scan_shifted = scan['scan_amp_b'].shift()
 scan['scan_amp_b_prev'] = scan_shifted
+
 
 def event_no(row):
     global start_event_no
@@ -39,11 +49,14 @@ def event_no(row):
     else:
         return start_event_no
 
+
 scan['event_no'] = scan.apply(lambda x: event_no(x), axis=1)
 
 scan_dwell = scan.groupby('event_no').agg({'period_scan': 'sum', 'scan_amp_b': 'max'})
 scan_dwell['time_scan'] = scan_dwell['period_scan'].cumsum()
 
 print(scan_dwell)
+# below some magic numbers to scale amplitude and shift time, this should be read from scn/prt
 g = sns.relplot(kind='line', x="time_scan", y="scan_amp_b", data=scan_dwell, drawstyle='steps-pre', color='grey')
+g.map(sns.lineplot, x=(abf.sweepX - 0.0617)*1000, y=(abf.sweepY -1)/2.5)
 plt.show()
