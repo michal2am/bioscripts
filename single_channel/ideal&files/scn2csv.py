40ce93584663d3b15a26244cc98313acd3b0e7e4
michal2am <michaladammichalowski@gmail.com>
2023-06-23 14:08:57 +0200

    fix merge scan plot

--- single_channel/ideal&files/scn2csv.py
+++ single_channel/ideal&files/scn2csv.py
@@ -64,6 +64,22 @@ scan_dwell.loc[scan_dwell['period_scan'] >= 0.05, 'under_res'] = -0.2
 print(scan_dwell)
 scan_dwell.to_csv(args.out_file, index=False)
 
+clampfit_dwell_09 = pd.read_csv('WT_25_C1_Clampfit_09.csv')
+clampfit_dwell_07 = pd.read_csv('WT_25_C1_Clampfit_07.csv')
+clampfit_dwell_05 = pd.read_csv('WT_25_C1_Clampfit_05.csv')
+
+clampfit_dwell_095 = pd.read_csv('WT_25_C1_Clampfit_095.csv')
+clampfit_dwell_10 = pd.read_csv('WT_25_C1_Clampfit_10.csv')
+clampfit_dwell_105 = pd.read_csv('WT_25_C1_Clampfit_105.csv')
+clampfit_dwell_11 = pd.read_csv('WT_25_C1_Clampfit_11.csv')
+
+clampfit_dwell_10_20 = pd.read_csv('WT_25_C1_Clampfit_10_20p.csv')
+clampfit_dwell_11_20 = pd.read_csv('WT_25_C1_Clampfit_11_20p.csv')
+
+# TODO: 0.95 == 1.6492
+
+#print(clampfit_dwell)
+
 if args.plot_abf_scn:
 
     # upward currents are openings
@@ -79,6 +95,20 @@ if args.plot_abf_scn:
 
     # below some magic numbers to scale amplitude and shift time, this should be read from scn/prt
     g = sns.relplot(kind='line', x="time_scan", y="scan_amp_b", data=scan_dwell, drawstyle='steps-pre', color='grey')
-    g.map(sns.lineplot, x=(abf.sweepX - abf_time_shift)*1000, y=((abf.sweepY - abf_amplitude_shift )/abf_amplitude))
     g.map(sns.lineplot, x=scan_dwell.time_scan, y=scan_dwell.under_res, drawstyle='steps-pre', color='grey')
+    # abf recording:
+    g.map(sns.lineplot, x=(abf.sweepX - abf_time_shift)*1000, y=((abf.sweepY - abf_amplitude_shift )/abf_amplitude))
+    # clampfit ideal:
+    # g.map(sns.lineplot, x=clampfit_dwell_05.Start - 59.7, y=(clampfit_dwell_05.Level/10) + 1.05, drawstyle='steps-post', color='red')
+    # g.map(sns.lineplot, x=clampfit_dwell_07.Start - 59.7, y=(clampfit_dwell_07.Level/10) + 1.16, drawstyle='steps-post', color='red')
+    # g.map(sns.lineplot, x=clampfit_dwell_09.Start - 59.7, y=(clampfit_dwell_09.Level/10) + 1.27, drawstyle='steps-post', color='red')
+
+    #g.map(sns.lineplot, x=clampfit_dwell_095.Start - 59.7, y=(clampfit_dwell_095.Level/10) + 1.38, drawstyle='steps-post', color='red')
+    g.map(sns.lineplot, x=clampfit_dwell_10.Start - 59.7, y=(clampfit_dwell_10.Level/10) + 1.49, drawstyle='steps-post', color='red')
+    g.map(sns.lineplot, x=clampfit_dwell_10_20.Start - 59.7, y=(clampfit_dwell_10_20.Level / 10) + 1.60,drawstyle='steps-post', color='red')
+    #g.map(sns.lineplot, x=clampfit_dwell_105.Start - 59.7, y=(clampfit_dwell_105.Level/10) + 1.60, drawstyle='steps-post', color='red')
+    g.map(sns.lineplot, x=clampfit_dwell_11.Start - 59.7, y=(clampfit_dwell_11.Level/10) + 1.71, drawstyle='steps-post', color='red')
+    g.map(sns.lineplot, x=clampfit_dwell_11_20.Start - 59.7, y=(clampfit_dwell_11_20.Level/10) + 1.82, drawstyle='steps-post', color='red')
+
+
     plt.show()
