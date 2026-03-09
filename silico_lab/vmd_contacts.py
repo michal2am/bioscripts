89c3e888f787a568063df1e54bb6c4c22dad365b
michal2am <michaladammichalowski@gmail.com>
2026-03-09 15:43:15 +0100

    vmd contacts parser

--- silico_lab/vmd_contacts.py
+++ silico_lab/vmd_contacts.py
@@ -1,8 +1,16 @@
+import numpy as np
 import pandas as pd
+import plotly.graph_objects as go
+import plotly.express as px
+
 
 # is StartPres really a start?
 
-contacts = pd.read_csv('contacts.csv')
+contactsAB = pd.read_csv('contacts_AB_sys2.csv', dtype={'PresenceVector': str})
+contactsCD = pd.read_csv('contacts_CD_sys2.csv', dtype={'PresenceVector': str})
+
+contacts = pd.concat([contactsAB, contactsCD], ignore_index=True)
+
 contacts["PresenceList"] = contacts["PresenceVector"].apply(lambda s: [int(c) for c in s])
 #print(contacts.columns)
 
@@ -20,16 +28,61 @@ pd.set_option('display.width', None)
 to_print = contacts[["ChainA", "ResnameA","ResidA", "ChainB", "ResnameB", "ResidB", "Type", "StartPres", "EquiStab200", "EquiStab", "RunStab"]]
 
 print(to_print[(contacts['StartPres'] == 1)
-               & (contacts['EquiStab200'] < 0.9)
-               & (contacts['EquiStab'] < 1)
-               & (contacts['RunStab'] < 0.5)
+               & (contacts['EquiStab200'] < 0.75)
+               & (contacts['EquiStab'] < 1.01)
+               & (contacts['RunStab'] < 1.01)
                & (contacts['ResidA'] < 220)])
 
 # here EquiStab should exclude EquiStab200 part ...
 print(to_print[(contacts["StartPres"] == 1)
-               & (contacts['ResidA'] < 200)
+               & (contacts['ResidA'] < 220)
                & (contacts['EquiStab'] < 0.75*contacts['EquiStab200'])])
 
 print(to_print[(contacts["StartPres"] == 1)
-               & (contacts['ResidA'] < 200)
+               & (contacts['ResidA'] < 220)
                & (contacts['RunStab'] < 0.75*contacts['EquiStab'])])
+
+selected_contacts = to_print[(to_print['StartPres'] == 1)
+                     & (to_print['EquiStab200'] < 1.01)
+                     & (to_print['EquiStab'] < 1.01)
+                     & (to_print['RunStab'] < 1.01)
+                     & (to_print['ResidA'] < 220)]
+
+# Build x-axis labels: ResnameA_ResidA – ResnameB_ResidB
+selected_contacts["label"] = ( selected_contacts["ResnameA"] + " " + selected_contacts["ResidA"].astype(str) + " – "
+                               + selected_contacts["ResnameB"] + " " + selected_contacts["ResidB"].astype(str))
+
+selected_contacts['BS'] = np.where(selected_contacts['ChainA'].isin(['A', 'B']), "1st", "2nd")
+
+selected_contacts.sort_values(['ResidA', 'ResidB'], inplace=True)
+print(selected_contacts)
+
+# Melt the three stability columns into long format
+df_melted = selected_contacts.melt(
+    id_vars=["label", "Type", "ResidA", "ResidB", "BS"],
+    value_vars=["EquiStab200", "EquiStab", "RunStab"],
+    var_name="Metric",
+    value_name="Stability",
+)
+
+df_melted.sort_values(['ResidA', 'ResidB'], inplace=True)
+ordered_labels = df_melted['label'].tolist()
+
+
+fig = px.bar(
+    df_melted,
+    x="label",
+    y="Stability",
+    color="Metric",
+    facet_row="BS",
+    facet_col="Type",
+    barmode="group",
+    title="Interaction Stability per Residue Pair",
+    labels={"label": "Residue A – Residue B"},
+    height=700,
+    template="plotly_white",
+    category_orders={"label": ordered_labels},
+)
+
+fig.update_xaxes(tickangle=-45)
+fig.show()
